c380ba7800890a24d5eb37a43797decb68f4df24
zkxjzmswkwl <smithcarterfreelance@gmail.com>
2021-10-16 05:45:08 -0600

    Add Account model. Declare initial apps in Django settings. Migrate.

--- Account/models.py
+++ Account/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    # AID
+    account_id = models.IntegerField()
+
+    # UserID
+    user_id = models.CharField(max_length=32)
+
+    # UGradeID
+    ugrade = models.IntegerField(default=0)
+
+    # PGradeID
+    pgrade = models.IntegerField(default=0)
+
+    # RegDate
+    reg_date = models.DateTimeField()
+
+    # Country 
+    country = models.CharField(max_length=32)
+
+    # LastLoginTime
+    last_login = models.DateTimeField()
+
+    # LastLogoutTime
+    last_logout = models.DateTimeField()
+
+    # IsBanned
+    is_banned = models.BooleanField(default=False)
+
+    # BannedReason
+    ban_reason = models.CharField(max_length=280)
