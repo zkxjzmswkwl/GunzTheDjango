bea546d8a9cef6534b0c15d75856a4725c1fc0dc
zkxjzmswkwl <smithcarterfreelance@gmail.com>
2021-10-16 05:49:06 -0600

    Character model.

--- Character/models.py
+++ Character/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from Account.models import Account
 
-# Create your models here.
+
+class Character(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+    character_id = models.IntegerField()
+
+    name = models.CharField(max_length=16)
+
+    level = models.IntegerField()
+
+    bounty = models.IntegerField(default=0)
+
+    kills = models.IntegerField()
+
+    deaths = models.IntegerField()
+
+    reg_date = models.DateTimeField()
+
+    delete_flag = models.IntegerField(default=0)
